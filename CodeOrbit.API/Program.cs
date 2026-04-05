c56d409e3cb1b3137d89a79e7e9530f188657979
Sumeyye Koyuncu <118927227+sumeyyekoyuncu@users.noreply.github.com>
2026-04-05 20:44:14 +0300

    Enable Swagger in all environments

--- CodeOrbit.API/Program.cs
+++ CodeOrbit.API/Program.cs
@@ -92,11 +92,10 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
