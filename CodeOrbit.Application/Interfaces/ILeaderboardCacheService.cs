5e9303655e14e31f5e45bbe20a202fc9a9b42758
sumeyyekoyuncu <sumeyye.koyuncu@icloud.com>
2026-04-06 10:42:02 +0300

    feat: add Redis cache layer for leaderboard service

---
+++ CodeOrbit.Application/Interfaces/ILeaderboardCacheService.cs
@@ -0,0 +1,23 @@
+﻿using CodeOrbit.Application.DTOs.Leaderboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOrbit.Application.Interfaces
+{
+    public interface ILeaderboardCacheService
+    {
+        Task<List<LeaderboardEntryDto>?> GetGlobalLeaderboardAsync();
+        Task SetGlobalLeaderboardAsync(List<LeaderboardEntryDto> data);
+
+        Task<List<LeaderboardEntryDto>?> GetWeeklyLeaderboardAsync();
+        Task SetWeeklyLeaderboardAsync(List<LeaderboardEntryDto> data);
+
+        Task<List<LeaderboardEntryDto>?> GetStreakLeaderboardAsync();
+        Task SetStreakLeaderboardAsync(List<LeaderboardEntryDto> data);
+
+        Task InvalidateAllAsync();
+    }
+}
